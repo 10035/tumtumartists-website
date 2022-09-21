0dae8ac668efab9537d462e40ccd38c5c3f1600d
Luis <looes1@gmail.com>
2022-09-21 16:28:11 +1000

    add basic layout styling to navbar

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,14 +1,15 @@
 import { Link } from 'gatsby'
 import React from 'react'
+import * as Navbar from '../styles/navbar.module.css'
 
 function navbar() {
   return (
-    <div className="links">
-        <Link to="/">About Us</Link>
-        <Link to="/artists">For Artists</Link>
-        <Link to="/fans">For Fans</Link>
-        <Link to="/portfolio">Portfolio</Link>
-        <Link to="/contact">Contact Us</Link>
+    <div className={Navbar.nav}>
+        <Link to="/" className={Navbar.links}>About Us</Link>
+        <Link to="/portfolio" className={Navbar.links}>Portfolio</Link>
+        <Link to="/artists" className={Navbar.links}>For Artists</Link>
+        <Link to="/fans" className={Navbar.links}>For Fans</Link>
+        <Link to="/contact" className={Navbar.links}>Contact Us</Link>
     </div>
   )
 }
