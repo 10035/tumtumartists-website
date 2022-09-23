55e2c4763cf9ebca0374ffecbb09702de5806384
Luis <looes1@gmail.com>
2022-09-24 01:45:46 +1000

    style main page components

--- src/components/banner.js
+++ src/components/banner.js
@@ -1,9 +1,12 @@
 import React from 'react'
 import Banner from '../images/banner.png'
+import * as BannerStyle from '../styles/banner.module.css'
 
 function banner() {
   return (
-    <img src={Banner} alt="test" style={{ maxWidth: '100%' }}></img>
+    <div>
+        <img src={Banner} alt="test" className={BannerStyle.banner}></img>
+    </div>
   )
 }
 
