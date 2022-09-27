c9956286375e2e3a58ad643863a539f645f1d125
Luis <looes1@gmail.com>
2022-09-27 22:46:14 +1000

    enhance styles main page

--- src/pages/index.js
+++ src/pages/index.js
@@ -10,9 +10,21 @@ function index() {
      <main>
       <Layout>
         <Banner />
-        {/* <div className={Styles.body}>Body</div> */}
-          <iframe width="560" height="315" src="https://www.youtube.com/embed/BNro3uBlqVg" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-          <iframe width="560" height="315" src="https://www.youtube.com/embed/nN_0YlcN-i8" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+        {/* <div className={Styles.body}>Body</div> */} 
+        {/* TODO Fix Width to match page width */}
+          <div className={Styles.slogan}>
+            <h2> - When you Tum Tum, there is fun fun - </h2>
+          </div>
+          <section>
+            <iframe width="33.3%" height="315" src="https://www.youtube.com/embed/BNro3uBlqVg" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+            <iframe width="33.3%" height="315" src="https://www.youtube.com/embed/4I25nV9hXGA" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+            <iframe width="33.3%" height="315" src="https://www.youtube.com/embed/nN_0YlcN-i8" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+          </section>
+          <section>
+            <iframe width="33.3%" height="315" src="https://www.youtube.com/embed/5g2hT4GmAGU" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+            <iframe width="33.3%" height="315" src="https://www.youtube.com/embed/-PJF2k0kURI" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+            <iframe width="33.3%" height="315" src="https://www.youtube.com/embed/YaEG2aWJnZ8" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+          </section>
       </Layout>
      </main>
   )
